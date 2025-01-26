04ba8499734c1b55c46f6aedf23f5f41ad9d4bb0
saksham_gupta <sakshamg9131516817@gmail.com>
2025-01-26 17:53:59 +0530

    final change

--- Car_Crafter/settings.py
+++ Car_Crafter/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'Car_Crafter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone',
+        'USER': 'postgres',
+        'PASSWORD': '9174588607',
+        'HOST': 'localhost',
+        'PORT': '5432',
+}
+
 }
 
 
@@ -123,6 +128,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'Car_Crafter/static'),
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URl = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

439ab4f8b5c92c533a751fc04e3674b44b5011fc
saksham_gupta <sakshamg9131516817@gmail.com>
2025-01-26 14:22:09 +0530

    created

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('cars/', views.cars, name= 'cars'),
+    path('about/', views.about, name= 'about'),
+    path('services/', views.services, name= 'services'),
+    path('contact/', views.contact, name= 'contact'),
+
 ]

fa3fcb1d95f4eacad40f0102c063ed42764fb7ea
saksham_gupta <sakshamg9131516817@gmail.com>
2025-01-29 21:01:46 +0530

    a

--- Car_Crafter/settings.py
+++ Car_Crafter/settings.py
@@ -26,13 +26,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'FALSE').lower() == 'true'
 
 # Ensure ALLOWED_HOSTS is a list
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")\
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "car-crafter-gitproject.onrender.com"
-]
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     "127.0.0.1",
+#     "car-crafter-gitproject.onrender.com"
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://car-crafter-gitproject.onrender.com"
